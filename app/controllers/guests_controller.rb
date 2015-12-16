f2f0df47e06a1e92fdb4e4d0bde3dccd8b05d383
Ryan Leung <ryanleung@berkeley.edu>
2015-12-15 22:18:51 -0800

    Edit and create functionality.

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -10,6 +10,27 @@ class GuestsController < ApplicationController
   def new
   end
 
+  def edit
+    @guest = Guest.find(params[:id])
+  end
+
+  def destroy
+    @guest = Guest.find(params[:id])
+    @guest.destroy
+ 
+    redirect_to guests_path
+  end
+
+  def update
+    @guest = Guest.find(params[:id])
+   
+    if @guest.update(guest_params)
+      redirect_to @guest
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @guest = Guest.new(guest_params)
  
@@ -20,6 +41,6 @@ class GuestsController < ApplicationController
   private
 
   def guest_params
-    params.require(:guest).permit(:name, :plusOneAmount)
+    params.require(:guest).permit(:name, :plusOneAmount, :responded, :rsvpCode)
   end
 end
